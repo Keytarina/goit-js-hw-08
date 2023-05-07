5c01d853a947035bca840246d33d1d2f07468e5b
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-05-07 11:48:40 +0300

    Add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imageMarkup = createImageMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+gallery.addEventListener('click', onPhotoClick);
+
+function createImageMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `
+			<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" 
+                    	src="${preview}" 
+						data-source="${original}"
+                    	alt="${description}"
+					/>
+                </a>
+            </li>`;
+    })
+    .join('');
+}
+
+function onPhotoClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = SimpleLightbox.create(
+    `
+		<div class="modal">
+			<img src="${event.target.dataset.source}">
+		</div>
+	`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', event => {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+  instance.show();
+}
